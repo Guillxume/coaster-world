97afef4127c72b0143cf062f6e4f5fa20ffc3035
Guillxume <guillaume.kieffer.pro@gmail.com>
2023-03-28 14:58:48 +0200

    Essai exercice 2

--- tests/Controller/CoasterControllerTest.php
+++ tests/Controller/CoasterControllerTest.php
@@ -2,21 +2,25 @@
 
 namespace App\Tests\Controller;
 
+use App\Entity\Coaster;
+use App\Repository\UserRepository;
 use App\Repository\CoasterRepository;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class CoasterControllerTest extends WebTestCase
 {
-    public function testList(): void
+    
+    
+    public function testEdit(): void
     {
+        $coaster = new Coaster;
         $client = static::createClient();
-        $crawler = $client->request('GET', '/coaster');
-
+        $userRepository = static::getContainer()->get(UserRepository::class);
+        $user = $userRepository->findOneBy([
+            'email' => 'admin@monde.com',
+        ]);
+        $client->loginUser($user);
+        $client->request('GET','coaster'. $coaster->getId(). '/edit');
         $this->assertResponseIsSuccessful();
-
-        $coasterRepository = static::getContainer()->get(CoasterRepository::class);
-        $nodes = $crawler->filter('.coaster-list > .card');
-
-        $this->assertCount($coasterRepository->count([]), $nodes);
-    }
+}
 }
