6f4984cd5323b20daa7c375258ccd4ed42f84062
Guillxume <guillaume.kieffer.pro@gmail.com>
2023-03-28 13:44:11 +0200

    test pour la fonctionnalité de déconnexion

--- tests/Controller/LogoutTest.php
+++ tests/Controller/LogoutTest.php
@@ -10,7 +10,7 @@ class LogoutTest extends WebTestCase
     public function testLogout(): void
     {
         $client = static::createClient();
-        $crawler = $client->request('GET', '/logout');
+        $client->request('GET', '/logout');
         $this->assertSame(Response::HTTP_FOUND, $client->getResponse()->getStatusCode());
         $this->assertSame('http://localhost/', $client->getResponse()->headers->get('Location'));
         $client->request('GET', '/park/create');
