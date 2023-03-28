9b59fd010431489467938706b8259c2632cbfb33
Guillxume <guillaume.kieffer.pro@gmail.com>
2023-03-28 13:38:59 +0200

    test pour la fonctionnalité de déconnexion

---
+++ tests/Controller/LogoutTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
+
+class LogoutTest extends WebTestCase
+{
+    public function testLogout(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/logout');
+        $this->assertSame(Response::HTTP_FOUND, $client->getResponse()->getStatusCode());
+        $this->assertSame('http://localhost/', $client->getResponse()->headers->get('Location'));
+        $client->request('GET', '/park/create');
+        $this->assertSame(Response::HTTP_FOUND, $client->getResponse()->getStatusCode());
+        $this->assertSame('/login', $client->getResponse()->headers->get('Location'));
+    }
+    }
