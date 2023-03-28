cfd62a9b6a33dba70cbe84e38fa89dac770626ed
Guillxume <guillaume.kieffer.pro@gmail.com>
2023-03-28 13:51:44 +0200

    test sur la fonctionnalité de déconnexion

--- tests/Controller/LogoutTest.php
+++ tests/Controller/LogoutTest.php
@@ -10,11 +10,24 @@ class LogoutTest extends WebTestCase
     public function testLogout(): void
     {
         $client = static::createClient();
+        $crawler = $client->request('GET', '/login');
+        $form = $crawler->selectButton('Connexion')->form();
+        $form['email'] = 'test@test.com';
+        $form['password'] = 'password';
+        $client->submit($form);
+
+        // Vérifie que l'utilisateur est bien connecté
+        $this->assertTrue($client->getResponse()->isRedirect());
+
+        // Se déconnecter de l'application
         $client->request('GET', '/logout');
-        $this->assertSame(Response::HTTP_FOUND, $client->getResponse()->getStatusCode());
-        $this->assertSame('http://localhost/', $client->getResponse()->headers->get('Location'));
-        $client->request('GET', '/park/create');
+
+        // Vérifie que l'utilisateur est bien déconnecté
+        $this->assertTrue($client->getResponse()->isRedirect());
+
+        // On essaie d'accèder à une page qui nécessite d'être connecté
+        $client->request('GET', '/coaster/create');
         $this->assertSame(Response::HTTP_FOUND, $client->getResponse()->getStatusCode());
         $this->assertSame('/login', $client->getResponse()->headers->get('Location'));
     }
-    }
+}
